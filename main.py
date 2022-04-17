ade0cac8669c94fe261540b6ab0c0bb488578ddb
Rosalee Knight <97799058+RosaleeKnight@users.noreply.github.com>
2022-04-17 22:52:27 +0000

    add canvas

--- main.py
+++ main.py
@@ -17,4 +17,14 @@ class PaintGUI:
             self.brush_width = 15
             self.current_colour = "#000000"
 
+            self.cnv = Canvas(self.root, width=WIDTH-10, height=HEIGHT-10, bg="white")
+            self.cnv.pack()
+            self.cnv.bind("<B1-Motion>", self.paint)
+
+            self.image = PIL.Image.new("RGB", (WIDTH, HEIGHT), WHITE)
+            self.draw = ImageDraw(self.image)
+
             
+
+        def paint(self):
+            pass
